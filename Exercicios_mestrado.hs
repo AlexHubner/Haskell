0b8cf60a1375e02886868cb6607a30a970c48a6d
AlexHubner <86573571+AlexHubner@users.noreply.github.com>
2021-07-14 12:33:33 -0300

    Update Exercicios_mestrado.hs

--- Exercicios_mestrado.hs
+++ Exercicios_mestrado.hs
@@ -11,6 +11,9 @@ diferenca2 (x:xs) y
         | elem x y = diferenca2 xs y    
         | otherwise = x : (diferenca2 xs y)
 
+--    ESTUDO DE CASO: Seja uma lista x = [1,2,3,4] e uma lista y = [2,3,4,5], como seria uma lista contendo somente os elementos diferentes
+-- entre as duas listas? RESPOSTA ESPERADA: [1,5]
+
 --b) Retorna a interseção entre duas listas. O resultado é uma lista.
 
 inter xs ys = [a|a <- xs, elem a ys]
@@ -23,11 +26,15 @@ inter2 (x:xs) ys
       | x `elem` ys = x: inter2 xs ys
       | otherwise = inter2 xs ys
 
-
+--    ESTUDO DE CASO: Seja uma lista x = [1,2,3,4] e uma lista y = [2,3,4,5], como seria uma lista contendo a interseção
+--(elementos iguais) entre as duas listas? RESPOSTA ESPERADA: [2,3,4]
 
 --c) Retorna a união entre duas listas (pode haver repetição de elementos). O resultado é uma lista.
 
 uniao x y = x ++ y
+
+--    ESTUDO DE CASO:  Seja uma lista x = [1,2,3,4] e uma lista y = [2,3,4,5], como seria uma lista contendo os elementos de
+-- ambas as listas? RESPOSTA ESPERADA:
 
 --d) Retorna a união entre duas listas (não há repetições de elementos). O resultado é uma lista.
 
@@ -37,9 +44,14 @@ listaSemRepeticao x y = tr (uniao x y)
                                     | elem x xs = tr xs
                                     | otherwise = x : tr xs
 
+--    ESTUDO DE CASO:  Seja uma lista x = [1,2,3,4] e uma lista y = [2,3,4,5], como seria uma lista contendo os elementos de
+-- ambas as listas, mas sem a repetição dos elementos? RESPOSTA ESPERADA:
+
 --e) Retorna o último elemento de uma lista.
 
 ultimo xs = last xs
+
+--    ESTUDO DE CASO:  Seja uma lista x = [1,2,3,4], qual é o último elemento da lista?  RESPOSTA ESPERADA: 4
 
 --f) Retorna o n-ésimo elemento de uma lista.
 nelemento :: Int -> [a] -> a
@@ -48,9 +60,14 @@ nelemento e (x:xs)
     | e <= 0 = x
     | otherwise = nelemento (e - 1) xs
 
+--    ESTUDO DE CASO: Seja uma lista x = [1,2,3,4], qual seria o terceiro elemento dessa lista? RESPOSTA ESPERADA: 3
+
 --g) Inverte uma lista.
 
 inverter xs = reverse xs
+
+--    ESTUDO DE CASO: Seja uma lista x = [1,2,3,4], como ficaria essa mesma lista de forma invertida?
+-- RESPOSTA ESPERADA: [4,3,3,1]
 
 --h) Ordena uma lista em ordem descrescente, removendo as eventuais repetições de elementos.
 
@@ -60,7 +77,12 @@ ordemDec (x:xs) = ordemDec (ma ++ [x] ++ ordemDec mn)
                     where ma = [e | e <- xs, e > x]
                           mn = [e | e <- xs, e <= x]
 
+--    ESTUDO DE CASO: Dada uma lista x = [1,3,2,4,2] como fica essa lista em ordem decrescente e sem repetições em seus elementos?
+-- RESPOSTA ESPERADA: [4,3,2,1]
+
 --i) Retorna um booleano indicando se uma lista de inteiros é decrescente ou não. Proponha 3  soluções: 
+--    ESTUDO DE CASO: Seja uma lista x = [1,2,3,4] ela está ordenada de forma decrescente?
+-- RESPOSTA ESPERADA: Falso
 
 -- a) usando sort;
 
@@ -75,7 +97,10 @@ testaDec2 [a] = True
 testaDec2 (a:b:xs) = a >= b && testaDec2 (b:xs)
 
 
- --c) usando fold, map e zip.
- 
+--c) usando fold, map e zip.
+testaDec3 x = foldr (&&) (map (\(a,b) -> a>=b) (zip x)
 
- 
+--Entendendo:
+--foldr vai aplicar a função (&&) nas funções map e zip
+--map vai testar se um elemento é maior do que o outro
+--zip vai agrupando os elementos em pares
