2bfcc5914ab2ad043866a60ee2a0da8e4eb16a3d
AlexHubner <86573571+AlexHubner@users.noreply.github.com>
2021-07-15 16:56:10 -0300

    Update Slide_Listas_T1_4.hs

--- Slide_Listas_T1_4.hs
+++ Slide_Listas_T1_4.hs
@@ -6,7 +6,10 @@ produtoCartesiano x y = zip x y
 
 -- também ficou como exercício modelar matrizes com o lista de listas e definir operações sobre elas, por 
 -- exemplo soma e multiplicação.  
+sumList3 lista1 lista2 = zipWith (+) lista1 lista2
 
-mat = array ((1,1), (2,3)) [((1,1),4), ((1,2)0), ((1,3), 3), ((2,1),5), ((2,2),1) ((2,3),4)]
+somaMat :: Num a => [[a]] -> [[a]] -> [[a]]
+somaMat l1 l2 = zipWith sumList3 l1 l2
 
-somaMat :: (Int a, Int b) => Array (a,a) b -> Array (a,a) b -> Array (a,a) b
+--Estudo de caso: dada uma matriz L1 = [[1,2,3], [4,5,6]] e uma matriz L2 = [[3,2,1], [6,5,4]] retornar
+--uma matriz com a soma de cada elemento equivalente. Resultado: [[4,4,4], [10,10,10]]
