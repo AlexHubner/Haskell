73b4623929f822696c9ee2d67043046935b8f21d
AlexHubner <86573571+AlexHubner@users.noreply.github.com>
2021-07-29 23:02:32 -0300

    Update Slide_Listas_T1_4.hs

--- Slide_Listas_T1_4.hs
+++ Slide_Listas_T1_4.hs
@@ -2,14 +2,17 @@
 -- ficou como exercício implementar a função produtoCartesiano sem usar compreensão de listas
 
 produtoCartesiano :: [Int] -> [Int] -> [(Int,Int)]
-produtoCartesiano x y = zip x y
+produtoCartesiano _ [] = []
+produtoCartesiano [] _ = []
+produtoCartesiano (x:xs) (y:ys) = [(x,y) ++ produtoCartesiano [x] ys ++ produtoCartesiano xs ys ++ produtoCartesiano
 
 -- também ficou como exercício modelar matrizes com o lista de listas e definir operações sobre elas, por 
--- exemplo soma e multiplicação.  
-sumList3 lista1 lista2 = zipWith (+) lista1 lista2
+-- exemplo soma e multiplicação. 
+
+sumList lista1 lista2 = zipWith (+) lista1 lista2
 
 somaMat :: Num a => [[a]] -> [[a]] -> [[a]]
-somaMat l1 l2 = zipWith sumList3 l1 l2
+somaMat l1 l2 = zipWith sumList l1 l2
 
 --Estudo de caso: dada uma matriz L1 = [[1,2,3], [4,5,6]] e uma matriz L2 = [[3,2,1], [6,5,4]] retornar
 --uma matriz com a soma de cada elemento equivalente. Resultado: [[4,4,4], [10,10,10]]
